af0fff8056f5ee47aceed89a21f0df4bcec2386f
Arpit <arpitshukla993@gmail.com>
2023-09-22 21:10:57 +0530

    Add tweets and new hashtags from new tweets

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 const express = require('express');
 const connect = require('./config/database');
 const app = express();
-
-const Tweet = require('./models/tweet');
+const {TweetRepository} = require('./repository/tweet-repo');
+const TweetService = require('./service/tweet-service');
 
 app.listen(3000, async () => {
     console.log('server started');
     await connect();
     console.log('Mongo db connected');
-    const tweets = await Tweet.find({
-        content: ["12345678"]
-        
-    });
-    console.log(tweets);
 });
--- src/repository/index.js
+++ src/repository/index.js
@@ -1,3 +1,4 @@
 module.exports = {
-    TweetRespository: require('./tweet-repo')
+    TweetRespository: require('./tweet-repo'),
+    HashtagRepository:require('./hashtag-repo')
 }
