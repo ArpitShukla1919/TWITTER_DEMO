74390e22ccd20146c596fb09a6b2c148116a2257
Arpit <arpitshukla993@gmail.com>
2023-10-11 20:06:52 +0530

    like toggle bug fixed

--- src/service/like-service.js
+++ src/service/like-service.js
@@ -22,7 +22,7 @@ class LikeService {
         if(exists){
             likeable.likes.pull(exists.id);
             await likeable.save();
-            await exists.save();
+            await exists.deleteOne();
             var isAdded = false;
         }
         else{
